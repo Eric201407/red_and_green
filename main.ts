0daa0497de3ad1b8da3bfc0728e4727b53a557fa
Eric201407 <104667971+Eric201407@users.noreply.github.com>
2022-05-01 12:23:19 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+basic.forever(function () {
+    if (input.pinIsPressed(TouchPin.P2)) {
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        pins.digitalWritePin(DigitalPin.P1, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        pins.digitalWritePin(DigitalPin.P1, 0)
+    }
+})
